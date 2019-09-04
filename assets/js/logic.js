3fc8dda30f6a322513035f779b5e31ed190c3b5e
Jmo5896 <jmo5896@moore-clan.net>
2019-09-04 10:58:10 -0600

    trying to put together a basic skeleton and start writing game logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,13 @@
 $(document).ready(()=>{
 
+    function populateGameBoard(numStones, playerNum, slots=6) {
+        
+        
+        for (let i = 1; i <= slots; i++) {
+            let newDiv = $('<div>').addClass('card').attr('id', `p${playerNum}-${}`);
+            newDive.text(`stones: ${numStones}`)
+        }
+    }
 
 
 
