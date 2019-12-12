11a0a3c0d9ae50f61531fb2744b8a1d992d26307
jmo5896 <justin.m.moore5896@gmail.com>
2019-12-11 20:26:48 -0600

    changed the app.js to a class, added the score, thinking first make hot seat one v one

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    player1: 0,
+    player2: 0
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Moncola</h1>
+        <h2> player 1: {this.state.player1} | player 2: {this.state.player1}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
